e52fee627f74c42e3ddacf3dc67665632363e4c5
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-09-05 10:06:13 +0100

    Uncomment withdraw route in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
        productSales: resolve(__dirname, 'product-sales.html'),
         wallet: resolve(__dirname, 'wallet.html'),
         deposit: resolve(__dirname, 'deposit.html'),
-        //withdraw: resolve(__dirname, 'withdraw.html'),
+        withdraw: resolve(__dirname, 'withdraw.html'),
         //transactions: resolve(__dirname, 'transactions.html'),
       //  referrals: resolve(__dirname, 'referrals.html'),
         //giftCode: resolve(__dirname, 'gift-code.html'),
