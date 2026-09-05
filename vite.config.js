0a95326dd5621ffb981d193836fd075e85a17b07
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-09-05 10:00:27 +0100

    Enable deposit route in vite.config.js
    Uncomment deposit route in Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         myProducts: resolve(__dirname, 'my-products.html'),
        productSales: resolve(__dirname, 'product-sales.html'),
         wallet: resolve(__dirname, 'wallet.html'),
-        //deposit: resolve(__dirname, 'deposit.html'),
+        deposit: resolve(__dirname, 'deposit.html'),
         //withdraw: resolve(__dirname, 'withdraw.html'),
         //transactions: resolve(__dirname, 'transactions.html'),
       //  referrals: resolve(__dirname, 'referrals.html'),
