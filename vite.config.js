192bc616ce1f37eab8d935345b6f5a9ff4e1b4e0
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-09-06 09:12:49 +0100

    Uncomment adminTransactions in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
         adminSettlements: resolve(__dirname, 'admin/settlements.html'),
         adminDeposits: resolve(__dirname, 'admin/deposits.html'),
         adminWithdrawals: resolve(__dirname, 'admin/withdrawals.html'),
-       // adminTransactions: resolve(__dirname, 'admin/transactions.html'),
+        adminTransactions: resolve(__dirname, 'admin/transactions.html'),
         //adminReferrals: resolve(__dirname, 'admin/referrals.html'),
         //adminGiftCodes: resolve(__dirname, 'admin/gift-codes.html'),
         //adminNotifications: resolve(__dirname, 'admin/notifications.html'),
